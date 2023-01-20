27154301a72f3cc2ac315718d3272ae12c78e332
차 <122503455+hw-ch@users.noreply.github.com>
2023-01-20 03:42:31 +0000

    20230120 Exam3 Bigger

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
 class Main {
   public static void main(String[] args) {
-    // 삼각형의 면적 구하기
-    double base = 4;
-    double height = 2;
-    double area = base * height / 2;
-    System.out.println(area);
+    // 더 큰 수 출력
+    int a = 3;
+    int b = 2;
+    if (a > b) {
+      System.out.println("a가 큰수입니다.");
+    } else if(a<b){
+      System.out.println("b가 큰수입니다.");
+    } else{
+      System.out.println("a와 b는 동일합니다.");
+    }
   }
 }
